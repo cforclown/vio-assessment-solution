559b2532d91359f44610dc227c356573459b5f5e
hafis <hafisalrizal@gmail.com>
2023-12-20 22:18:17 +0700

    add more unit test for environment.ts

--- chat-app/jestSetup.ts
+++ chat-app/jestSetup.ts
@@ -8,6 +8,12 @@ process.env.PORT='55555';
 process.env.ALLOWED_ORIGINS='http://localhost:55556';
 process.env.API_VERSION='v1';
 
+process.env.DB_HOST='mongo'
+process.env.DB_PORT='27017'
+process.env.DB_USERNAME='root'
+process.env.DB_PASSWORD='root'
+process.env.DB_NAME='chat-app'
+
 process.env.SESSION_SECRET='LAAD1_SESSION_SECRET';
 
 process.env.ACCESS_TOKEN_SECRET = 'ACCESS_TOKEN_SECRET';
--- chat-app/src/utils/environment.test.ts
+++ chat-app/src/utils/environment.test.ts
@@ -54,6 +54,44 @@ describe('environment', () => {
     });
   });
 
+  describe('Environment.getDBConnectionString', () => {
+    it('should successfully return environment variable value', () => {
+      process.env.DB_HOST = 'mongo';
+      process.env.DB_PORT = '27017';
+      process.env.DB_USERNAME = 'root';
+      process.env.DB_PASSWORD = 'root';
+      expect(Environment.getDBConnectionString()).toEqual('mongodb://root:root@mongo:27017');
+
+      process.env.DB_HOST = 'mongo';
+      process.env.DB_PORT = '27017';
+      process.env.DB_USERNAME = '';
+      process.env.DB_PASSWORD = '';
+      expect(Environment.getDBConnectionString()).toEqual('mongodb://mongo:27017');
+
+      process.env.DB_HOST = 'mongo';
+      process.env.DB_PORT = '';
+      process.env.DB_USERNAME = '';
+      process.env.DB_PASSWORD = '';
+      expect(Environment.getDBConnectionString()).toEqual('mongodb://mongo');
+    });
+
+    it('should throw error when DB_HOST is not set', () => {
+      process.env.DB_HOST = ''; // empty string is falsy
+      expect(Environment.getDBConnectionString).toThrow(Error);
+    });
+  });
+
+  describe('Environment.getDbName', () => {
+    it('should successfully return environment variable value', () => {
+      expect(Environment.getDbName()).toBeTruthy();
+    });
+
+    it('should throw an error when environment variable not found', () => {
+      process.env.DB_NAME = ''; // empty string is falsy
+      expect(Environment.getDbName).toThrow(Error);
+    });
+  });
+
   describe('Environment.getAccessTokenExpIn', () => {
     it('should successfully return environment variable value', () => {
       process.env.ACCESS_TOKEN_EXP_IN = '1000';
