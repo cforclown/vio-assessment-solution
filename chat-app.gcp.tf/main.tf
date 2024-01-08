28c58b218f8c70a2ab4012c4d809a0acf8279f49
hafis <hafisalrizal@gmail.com>
2024-01-08 09:51:02 +0700

    add gcp terraform

--- chat-app.gcp.tf/main.tf
+++ chat-app.gcp.tf/main.tf
@@ -1,18 +1,16 @@
 provider "google" {
-  project = "chat-app"
+  project = "simple-chat-app"
   region  = "asia-southeast2"
 }
 
-resource "google_compute_network" "dev_network" {
-  name                    = "dev_network"
+resource "google_compute_network" "development_network" {
+  name                    = "development-network"
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "dev_subnet" {
-  name          = "dev_subnet"
+resource "google_compute_subnetwork" "dev_subnet_01" {
+  name          = "dev-subnet-01"
   ip_cidr_range = "10.100.0.0/16"
-  network       = google_compute_network.dev_network.id
+  network       = google_compute_network.development_network.id
   region        = "asia-southeast2"
 }
-
-
